2ecfbbf38db48e4641d5bcdbab3cd60ba16a7f9b
Shuo Liu <sliu@aviatrix.com>
2022-08-29 11:53:49 -0700

    Add docs, goreleaser, and GH actions

--- go.mod
+++ go.mod
@@ -4,5 +4,7 @@ go 1.13
 
 require (
 	github.com/hashicorp-demoapp/hashicups-client-go v0.0.0-20200508203820-4c67e90efb8e
+	github.com/hashicorp/terraform-plugin-docs v0.13.0 // indirect
 	github.com/hashicorp/terraform-plugin-sdk/v2 v2.0.0-rc.2
+	github.com/zclconf/go-cty v1.10.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,16 +1,19 @@
 package main
 
 import (
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
+    "github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+    "github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 
-	"terraform-provider-hashicups/hashicups"
+    "terraform-provider-hashicups/hashicups"
 )
 
+// Generate the Terraform provider documentation using `tfplugindocs`:
+//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
+
 func main() {
-	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: func() *schema.Provider {
-			return hashicups.Provider()
-		},
-	})
+    plugin.Serve(&plugin.ServeOpts{
+        ProviderFunc: func() *schema.Provider {
+            return hashicups.Provider()
+        },
+    })
 }
